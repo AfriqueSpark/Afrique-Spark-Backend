ba455127c1255897a05390a3fd58a7f006b6998e
Anuolu-2020 <Anuolu1645@gmail.com>
2024-01-30 18:54:03 +0100

    feat: connected to database

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 const dotenv = require("dotenv");
 const app = require("./app");
+const mongoose = require("mongoose");
 
 dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Database Connected Successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const PORT = process.env.PORT || 3000;
 
