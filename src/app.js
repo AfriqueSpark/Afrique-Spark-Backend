a67ef24769b9c2d821a70dc7a2063eddf7b14d91
Anuolu-2020 <Anuolu1645@gmail.com>
2024-01-30 18:53:47 +0100

    feat: implement users route middleware

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 
 const welcomeToApi = require("./middlewares/welcome.middleware");
-const checkApiVersion = require("./middlewares/checkApiVersion.middleware")
+
+const checkApiVersion = require("./middlewares/checkApiVersion.middleware");
+
+const { userRoute } = require("./routes/user.route");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/api/v:version", checkApiVersion, welcomeToApi);
+
+app.use("/api/v:version/users/", checkApiVersion, userRoute);
 
 app.all("*", (_, res) => {
   res.status(404).json({
