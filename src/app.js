fee2fa4753ff58187136a684f8af1c220df499b6
Anuolu-2020 <Anuolu1645@gmail.com>
2024-01-31 01:26:21 +0100

    feat: implement logging, session, auth route middleware and some security patches

--- src/app.js
+++ src/app.js
@@ -1,18 +1,78 @@
 const express = require("express");
+const passport = require("passport");
+const compression = require("compression");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const { v4: uuidv4 } = require("uuid");
+
+const session = require("express-session");
+const RedisStore = require("connect-redis").default;
+const redis = require("redis");
 
 const welcomeToApi = require("./middlewares/welcome.middleware");
-
 const checkApiVersion = require("./middlewares/checkApiVersion.middleware");
-
 const { userRoute } = require("./routes/user.route");
+const logger = require("./utils/logger");
+const authRoute = require("./routes/Auth/auth.route");
 
 const app = express();
 
 app.use(express.json());
 
+//Redis connection client
+const redisClient = redis.createClient({ url: process.env.REDIS_URL });
+
+//connect to redis
+redisClient.connect().catch(console.error);
+
+// Initialize store.
+let redisStore = new RedisStore({
+  client: redisClient,
+});
+
+// MANAGE COOKIE SESSIONS
+app.use(
+  session({
+    genid: () => {
+      return uuidv4();
+    },
+    store: redisStore,
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: false,
+      maxAge: 48 * 60 * 60 * 1000, // expires in two-days
+      secure: false,
+    },
+  })
+);
+
+//For setting secure headers
+app.use(helmet());
+
+//Initialize Passport
+app.use(passport.initialize());
+
+//Session for passport
+app.use(passport.session());
+
+//Prevent no-sql injection
+app.use(mongoSanitize({ allowDots: true }));
+
+//For compressing response body
+app.use(compression());
+
+//For logging
+app.use(logger());
+
 app.get("/api/v:version", checkApiVersion, welcomeToApi);
 
-app.use("/api/v:version/users/", checkApiVersion, userRoute);
+//USER'S ROUTES
+app.use("/api/v:version/users", checkApiVersion, userRoute);
+
+//AUTH ROUTES
+app.use("/api/v:version/auth", checkApiVersion, authRoute);
 
 app.all("*", (_, res) => {
   res.status(404).json({
@@ -21,4 +81,4 @@ app.all("*", (_, res) => {
   });
 });
 
-module.exports = app;
+module.exports = { app, redisClient };
