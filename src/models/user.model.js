08f216a044890393bf386af492c44ab2bacae999
Anuolu-2020 <Anuolu1645@gmail.com>
2024-02-03 22:25:53 +0100

    feat: added fullname and passwordConfirm properties to user schema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -5,6 +5,10 @@ const bcrypt = require("bcrypt");
 const schema = mongoose.Schema;
 
 const userSchema = new schema({
+  fullname: {
+    type: String,
+    required: [true, "Full name must be provided"],
+  },
   email: {
     type: String,
     required: [true, "An Email must be provided"],
@@ -14,6 +18,12 @@ const userSchema = new schema({
   password: {
     type: String,
     required: [true, "A password must be provided"],
+    minlength: 8,
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm your password"],
   },
   username: {
     type: String,
@@ -35,8 +45,12 @@ const userSchema = new schema({
 });
 
 // HASH PASSWORDS, THEN STORE IN DB
-userSchema.pre("save", async function () {
+userSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 //CHECK IF USER"S PASSWORD IS VALID
