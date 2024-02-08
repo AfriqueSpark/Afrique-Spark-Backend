654a751f4fdd82cf02f79b88dd52d67913e7ea9a
Anuolu-2020 <Anuolu1645@gmail.com>
2024-02-08 17:53:55 +0100

    Restructure: restructure vendor route

---
+++ src/routes/Products/vendor.route.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const multer = require("multer");
+
+const router = express.Router();
+
+const {
+  uploadProduct,
+} = require("../../controllers/Products/uploadProduct.controller");
+
+// Setting up multer as a middleware to grab photo uploads
+const storage = multer.memoryStorage();
+
+const upload = multer({ storage: storage });
+
+router
+  .route("/upload-product")
+  .post(upload.single("productImage"), uploadProduct);
+
+  module.exports = router;
