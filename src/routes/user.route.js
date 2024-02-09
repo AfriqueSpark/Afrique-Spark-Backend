855b1b38d6d71b93bacb10af25f95a2fcca2d4d8
Anuolu-2020 <Anuolu1645@gmail.com>
2024-02-09 17:32:06 +0100

    feat: implement update role route

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -6,8 +6,10 @@ const {
   getUserData,
   createUser,
   deleteUsers,
+  updateRole,
 } = require("../controllers/user.controller");
 
 userRoute.route("/").get(getUserData).post(createUser).delete(deleteUsers);
+userRoute.route("/update-role").put(updateRole);
 
 module.exports = { userRoute };
