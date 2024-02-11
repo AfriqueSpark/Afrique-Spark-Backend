adf172a38cf1ef207099f1b36c96b446aaeb9c8e
Anuolu-2020 <Anuolu1645@gmail.com>
2024-02-11 15:37:28 +0100

    feat: added route for removing product in cart

--- src/routes/Cart/cart.route.js
+++ src/routes/Cart/cart.route.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const addToCart = require("../../controllers/Cart/addToCart.controller");
 const getCart = require("../../controllers/Cart/getCart.controller");
+const removeFromCart = require("../../controllers/Cart/removeFromCart.controller");
 
 const router = express.Router();
 
 router.route("/addToCart").post(addToCart);
 router.route("/getCart").get(getCart);
+router.route("/remove/product/:productId").post(removeFromCart);
 
 module.exports = router;
